c3285a0a73bf5824fb412c50cbb0bbe03ac4c1c0
hhcol <2838967868@qq.com>
2020-10-13 19:28:10 +0800

    部署测试

---
+++ build/precache-manifest.789e7f29e35fbebd9dcb4b38c1bb403f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9e7f2e6ae4d3b22a02108d1614205a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db293698d4175eb507e4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e3bb42898bfc587c2007",
+    "url": "/static/js/2.744b708d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.744b708d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db293698d4175eb507e4",
+    "url": "/static/js/main.2275b7b3.chunk.js"
+  },
+  {
+    "revision": "ed9789d0b9c40fa96a07",
+    "url": "/static/js/runtime-main.6d06d1e9.js"
+  }
+]);
